d09253587fa786a4b74da6bc73d7606e7cbc8216
dayal <dashingdayal.s@gmail.com>
2023-03-02 03:47:24 +0530

    new experiments

---
+++ D.java
@@ -0,0 +1,12 @@
+class D{
+public void display(){
+System.out.println("Hello world");
+}
+public void show(){
+System.out.println("Wellcome to Hyd");
+}
+public void show(){
+System.out.println("system");
+}
+
+}
