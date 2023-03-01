2b71d4c686498450c5cd090ddd75a354d1f2892e
dayal <dashingdayal.s@gmail.com>
2023-03-02 03:36:41 +0530

    This is a Experimental file

---
+++ C.java
@@ -0,0 +1,12 @@
+class C{
+public void display(){
+System.out.println("Hello world");
+}
+public void show(){
+System.out.println("Wellcome to Hyd");
+}
+public void show(){
+System.out.println("system");
+}
+
+}
